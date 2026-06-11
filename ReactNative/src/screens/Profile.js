32739fb05870a71e1bd4a35b8be8f23317b6d30e
lhermida-prog <lhermida@udesa.edu.ar>
2026-06-11 16:51:07 -0300

    Avanvces en profile

--- ReactNative/src/screens/Profile.js
+++ ReactNative/src/screens/Profile.js
@@ -2,45 +2,60 @@ import React, { Component, useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
 import { auth, db } from '../FireBase/Config'
 import { Pressable } from 'react-native'
+import Post from '../components/Post'
 
-function Profile (props) {
-   const [nombre_usuario , setNombreUsuario] =   useState ([])
-   const [nombre_email , setNombreEmail] = useState ([])
-   const [publicaciones , setPublicaciones] = useState ([])
+function Profile(props) {
+  const [nombre_usuario, setNombreUsuario] = useState([])
+  const [nombre_email, setNombreEmail] = useState([])
+  const [publicaciones, setPublicaciones] = useState([])
 
-   function logout(){
+  function logout() {
     auth.signOut()
     props.navigation.navigate("Login")
-   }
-
-  useEffect (() => { db.collection ("users").where("owner", "==", auth.currentUser.email).onSnapshot(
-          docs => {
-              let usuarioss = []
-              docs.forEach (doc => {
-                  usuarioss.push({
-                      id : doc.id ,
-                      data : doc.data()
-  
-                  })
-                  setNombreUsuario(usuarioss.data.username)
-                  setNombreEmail(usuarioss.data.email)
-                  
-                  
-  
-              })
-          } ,
-          
-      )},[] )
-      
-    return (
-      <View>
-        <Text> Tu perfil </Text>
-        <Text>Usuario: {nombre_usuario}</Text>
-        <Pressable onPress={() => logout()}><Text >Cerrar Sesion</Text></Pressable>
-      </View>
-    )
   }
 
+  useEffect(() => {
+  
+    db.collection("users").where("email", "==", auth.currentUser.email).onSnapshot(
+      docs => {
+        let usuarioss = []
+        docs.forEach(doc => {
+          usuarioss.push({
+            id: doc.id,
+            data: doc.data()
+          })
+          
+          setNombreUsuario(usuarioss[0].data.username)
+          setNombreEmail(usuarioss[0].data.email)
 
-  export default Profile
+        })
+      },
+
+    )
+  }, [])
+  db.collection("Post").where("owner" , "==" , auth.currentUser.owner).onSnapshot(
+    docs => {
+      let posts = []
+      docs.forEach( doc => {
+        posts.push({
+          id: doc.id,
+          data: doc.data()
+        })
+        console.log(posts);
+      })
+    }
+  )
+
+  return (
+    <View>
+      <Text> Tu perfil </Text>
+      <Text> Usuario: {nombre_usuario}</Text>
+      <Text> Email: {nombre_email}</Text>
+      <Pressable onPress={() => logout()}><Text>Cerrar Sesion</Text></Pressable>
+    </View>
+  )
+}
+
+
+export default Profile
 
