ad51eaf5f473370ed7425d892e1e492701f5a7e6
gonza lopardo <glopardob@gmail.com>
2026-06-11 16:11:45 -0300

    Avances en comentarios y Likes terminados

--- ReactNative/src/Navigation/Tab/Tab.js
+++ ReactNative/src/Navigation/Tab/Tab.js
@@ -6,6 +6,8 @@ import Crear_Post from "../../screens/Crear_Post";
 import Entypo from '@expo/vector-icons/Entypo';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Comentarios from "../../screens/Comentarios";
+import StackHome from "../StackHome/Stackhome";
 
 
 
@@ -15,9 +17,10 @@ function Tabs (){
     return(
         
         <Tab.Navigator screenOptions={ { tabBarShowLabel: false } }>
-            <Tab.Screen name="Home" component = {Home} options={ { tabBarIcon: () => <Entypo name="home" size={24} color="black"/> }} />
+            <Tab.Screen name="StackHome" component = {StackHome} options={ { tabBarIcon: () => <Entypo name="home" size={24} color="black"/> }} />
             <Tab.Screen name = "Profile" component={Profile} options={ { tabBarIcon: () => <MaterialCommunityIcons name="face-man-profile" size={24} color="black" />} }/>
             <Tab.Screen name = "Crear_Post" component={Crear_Post} options={ { tabBarIcon: () =><AntDesign name="camera" size={24} color="black" />}}/>
+
         </Tab.Navigator>
         
     )
--- ReactNative/src/screens/Comentarios.js
+++ ReactNative/src/screens/Comentarios.js
@@ -1,51 +1,54 @@
-import { useState,useEffect } from "react";
-import { View,Text,TextInput,Pressable,FlatList} from 'react-native';
-import {db,auth} from '../FireBase/Config';
+import { useState, useEffect } from "react";
+import { View, Text, TextInput, Pressable, FlatList } from 'react-native';
+import { db, auth } from '../FireBase/Config';
 
-function Comentarios(props){
-    const [comentario,setcomentario] = useState('');
-    const [comentarios,setcomentarios] = useState('');
+function Comentarios(props) {
+    const [comentario, setcomentario] = useState('');
+    const [comentarios, setcomentarios] = useState('');
 
 
 
-    function agregarComentario(){
+    function agregarComentario() {
         db.collection('comentarios').add({
             idPost: idpost,
             comentario: comentario,
             email: auth.currentUser.email,
             createdAt: Date.now()
         })
-.then(() => {
-    setcomentario('')
-    })
-    
-  .catch((error) => {
-        console.log(error);
-     } )
-}
-
-useEffect(() =>{
-    db.collection('comentarios').where("idPost","==","").onSnapshot(
-        docs =>{
-            let listacomentarios = [];
-            docs.foreach(doc =>
-            {
-            comentarios.push({
-               id: doc.data() 
+            .then(() => {
+                setcomentario('')
             })
-        setcomentario(listacomentarios)
-            })
-        })})
-    
-        return(
-              <View>
 
-       
-   </View>
-        );
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
-        
+    useEffect(() => {
+        db.collection('comentarios').where("idPost", "==", "").onSnapshot(
+            docs => {
+                let listacomentarios = [];
+                docs.foreach(doc => {
+                    comentarios.push({
+                        id: doc.data()
+                    })
+                    setcomentario(listacomentarios)
+                })
+            })
+    })
+
+    return (
+        <View>
+
+
+        </View>
+    );
+}
+
+
+export default Comentarios
+
+
 
 
 
